48c6bb21dc16fdea144757db494bc5227f055d6b
mernstackdeveloper <49354823+mernstackdeveloper@users.noreply.github.com>
2020-05-12 23:03:38 +0530

    pure javascript calculator

---
+++ Pure javascript calculator/script.js
@@ -0,0 +1,63 @@
+var clickedButton,
+  equationDone = false;
+window.onload = function () {
+  $("button").click(function () {
+    clickedButton = $(this).attr("id");
+    click();
+  });
+};
+
+function click() {
+  if (clickedButton === "clear")
+    document.getElementById("inputField").innerHTML = "0";
+  else if (
+    clickedButton === "divi" ||
+    clickedButton === "multi" ||
+    clickedButton === "sub" ||
+    clickedButton === "dot" ||
+    clickedButton === "add"
+  ) {
+    if (document.getElementById("inputField").innerHTML === "Error") clear();
+    document.getElementById("inputField").innerHTML += document.getElementById(
+      clickedButton
+    ).innerHTML;
+    equationDone = false;
+  } else if (clickedButton === "equal") {
+    var equation = document.getElementById("inputField").innerHTML;
+    var answer;
+
+    try {
+      eval(equation);
+      answer = eval(equation);
+    } catch (e) {
+      if (e instanceof SyntaxError) {
+        document.getElementById("inputField").innerHTML = "Error";
+      }
+    }
+
+    if (answer == "Infinity")
+      document.getElementById("inputField").innerHTML = "Error";
+    else if (Number.isInteger(answer))
+      //if it's an int don't round to two
+      document.getElementById("inputField").innerHTML = answer;
+    else document.getElementById("inputField").innerHTML = answer.toFixed(2);
+
+    equationDone = true;
+  } else {
+    clear();
+    document.getElementById("inputField").innerHTML += document.getElementById(
+      clickedButton
+    ).innerHTML;
+  }
+}
+
+function clear() {
+  if (
+    document.getElementById("inputField").innerHTML === "0" ||
+    equationDone === true ||
+    document.getElementById("inputField").innerHTML === "Error"
+  )
+    document.getElementById("inputField").innerHTML = "";
+
+  equationDone = false;
+}
